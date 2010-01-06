cfb8008cb68d22a1a83e088f06765731f5ca93e7
doug tangren <d.tangren@gmail.com>
2010-01-06 02:25:27 -0500

    updating tests

--- src/test/scala/dispatch/FoursquareSpec.scala
+++ src/test/scala/dispatch/FoursquareSpec.scala
@@ -39,21 +39,30 @@ object FoursquareSpec extends Specification {
       C.config.getString("oauth_access_key").get, 
       C.config.getString("oauth_access_secret").get
     )
-    val client = OAuthClient(consumer, accessToken)
+    val fs = OAuthClient(consumer, accessToken)
     
    detailedDiffs()
     
     implicit val http = new Http
       
+    val Tokyo = 120l
+    val TimesSquareVenue = 41422
+    case class FSCity(id: Long, name: String, shortName: String, timezone: String, geo:(Double,Double))
+    val TestCity = FSCity(22, "New York", "NYC", "America/New_York", (40.759011, -73.984472))  
+    case class FSUser(id: Long, firstname: String, lastname: String)
+    val AFriend = FSUser(15026,"Ryan","Gravener")
+    val Me = FSUser(140048,"doug","tangren")
+      
     "Cities" should {
       "should find recently active cities" in {
-         val res = client.call(Cities recentlyActive)
+         val res = fs.call(Cities recentlyActive)
          val ids = for (r <- res; id <- City.id(r)) yield id
+         //val names = for (r <- res; name <- City.name(r)) yield name
+         //println("cities %s" format(ids zip names))
          ids.size must be > (0)
        }
       "find the foursquare city nearest 40.759011,-73.984472 (nyc)" in {
-        val res = client.call(Cities near(40.759011,-73.984472)) 
-       
+        val res = fs.call(Cities near(TestCity.geo))
         val List(name) = res.flatMap(City.name)
         val List(id) = res.flatMap(City.id)
         val List(shortName) = res.flatMap(City.shortName)
@@ -61,30 +70,41 @@ object FoursquareSpec extends Specification {
         //val List(geolat) = res.flatMap(City.geolat)
         //val List(geolong) = res.flatMap(City.geolong)
         
-        name must_== "New York"
-        id must_== 22
-        shortName must_== "NYC"
-        timezone must_== "America/New_York"
-        //geolat must be closeTo(40.759011, 0.003)
-        //geolong must be closeTo(-73.984472, 0.003)
+        name must_== TestCity.name
+        id must_== TestCity.id
+        shortName must_== TestCity.shortName
+        timezone must_== TestCity.timezone
+        //geolat.toString.toDouble must be closeTo(TimesSquareGeo._1, 0.003)
+        //geolong.toString.toDouble must be closeTo(TimesSquareGeo._2, 0.003)
+      }
+      "switch to other cities" in {
+        List(TestCity.id, Tokyo).foreach((id: Long) => {
+          val res = fs.call(Cities switch(id))
+          val List(status) = res.flatMap(CitySwitch.status)
+          status must_== "1"
+        })
       }
     }
-    
+  
     "Checkins" should {
       "perform a checkin" in {
-        // note you prob what to delete these from your history after running 
-        // this test :)
-        val res = client.call(Checkins.checkIn at("Times Square") shouting("testing dispatch-foursquare") privately)
+        val res = fs.call(Checkins
+          .checkIn 
+          .at("Times Square") 
+          .shouting("testing dispatch-foursquare") 
+          .privately
+        )
         val List(msg) = res.flatMap(Checkin.message)
         msg must_=="OK! We've got you @ Times Square."
       }
       "find checkins near 40.759011,-73.984472 (nyc)" in {
-          val res = client.call(Checkins near(40.759011,-73.984472))
+          val res = fs.call(Checkins near(TestCity.geo))
           val ids = for (r <- res; id <- Checkin.id(r)) yield id
+          //val fnames = for (r <- res; List(u) <- r; fname <- User.firstname(u.asInstanceOf[jValue])) yield fname
           ids.size must be > 0
       }
       "list a checkin history" in {
-        val res = client.call(Checkins history)
+        val res = fs.call(Checkins history)
         val ids = for (r <- res; id <- Checkin.id(r)) yield id
         ids.size must be > 0
       }
@@ -92,41 +112,88 @@ object FoursquareSpec extends Specification {
     
     "Users" should {
        "get the user info for 15026" in {
-        val res = client.call(Users get(15026))
-        val List(id) = res.flatMap(User.id)
-        id must_== 15026
+          val res = fs.call(Users get(15026))
+          val List(id) = res.flatMap(User.id)
+          val List(firstname) = res.flatMap(User.firstname)
+          val List(lastname) = res.flatMap(User.lastname)
+          firstname must_== AFriend.firstname
+          lastname must_== AFriend.lastname
+          id must_== AFriend.id
+        }
+        "get the user info about the current user" in {
+          val res = fs.call(Users current)
+          val List(id) = res.flatMap(User.id)
+          val List(firstname) = res.flatMap(User.firstname)
+          val List(lastname) = res.flatMap(User.lastname)
+          firstname must_== Me.firstname
+          lastname must_== Me.lastname
+          id must_== Me.id
+        }
       }
-      "get the user info about the current user" in {
-        val res = client.call(Users current)
-        val List(id) = res.flatMap(User.id)
-        id must_== 140048
-      }
-    }
+    // 
+    // "Friends" should {
+    //   "find the current user's friends" in {
+    //     val res = fs.call(Friends ofMe)
+    //     val ids = for (r <- res; id <- Checkin.id(r)) yield id
+    //     ids.size must be > 0
+    //     // todo check settings
+    //   }
+    //   "find user 140048's friends" in {
+    //     val res = fs.call(Friends of(140048))
+    //     val ids = for (r <- res; id <- Checkin.id(r)) yield id
+    //     ids.size must be > 0
+    //   }
+    //   "find friends by name" in {
+    //     val res = fs.call(Friends named("Ryan"))
+    //     val ids = for (r <- res; id <- Checkin.id(r)) yield id
+    //     ids must contain(15026) 
+    //   }
+    // }
     
-    "Friends" should {
-      "find user 140048's friends" in {
-        val res = client.call(Friends of(140048))
-        val ids = for (r <- res; id <- Checkin.id(r)) yield id
-        ids.size must be > 0
-      }
-      "find friends by name" in {
-        val res = client.call(Friends named("Ryan"))
-        val ids = for (r <- res; id <- Checkin.id(r)) yield id
-        ids must contain(15026) 
-      }
-    }
+    // "Venues" should {
+    //   "find the venues near 40.759011,-73.984472 (nyc)" in {
+    //     val res = fs.call(Venues near(TestCity.geo))
+    //     val ids = for (r <- res; id <- Venue.id(r)) yield id
+    //     ids.size must_== 0
+    //   }
+    //   "get venue details for nyc times square" in {
+    //     val res = fs.call(Venues get(TestCity.geo))
+    //     val List(id) = res.flatMap(Venue.id)
+    //     id must_== 41422
+    //   }
+    //   "add a venue" {
+    //     val res = fs.call(Venues.add
+    //       .name("Doug's apartment")
+    //       .address("408 E 88th st")
+    //       .crossstreet("1st & 88th")
+    //       .city("New York")
+    //       .state("NY")
+    //     )
+    //     val List(name) = res.flatMap(Venue.name)
+    //     name must_== "Doug's apartment"
+    //   }
+    //   "close a venue" {
+    //     val res = fs.call(Venues close())
+    //     val List(msg) = res
+    //     msg must_== "ok"
+    //   }
+    // }
     
-    "Venues" should {
-      "find the venues near 40.759011,-73.984472 (nyc)" in {
-        val res = client.call(Venues near(40.759011,-73.984472))
-        val ids = for (r <- res; id <- Venue.id(r)) yield id
-        ids.size must_== 0
-      }
-      "get venue details for nyc times square" in {
-        val res = client.call(Venues get(41422))
-        val List(id) = res.flatMap(Venue.id)
-        id must_== 41422
-      }
-    }
+    // "Tips" should {
+    //   "find tips near 40.759011,-73.984472 (nyc)" in {
+    //     val res = fs.call(Tips near(TestCity.geo))
+    //     val ids = for (r <- res; id <- Tip.id(r)) yield id
+    //     ids.size must be > 0
+    //   }
+    //   "add a new tip at 40.759011,-73.984472" in {
+    //     val res = fs.call(Tip.add
+    //       .at(TimesSquareGeo) 
+    //       .text("test tip")
+    //       .asTip
+    //     )
+    //     val List(txt) = res.flatMap(Tip.text)
+    //     txt must_=="test tip"
+    //   }
+    // }
   }
 }
